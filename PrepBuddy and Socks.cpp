2875b2afe6301359f0b50dae29f54812bd163260
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-12-10 22:29:12 +0530

    Create PrepBuddy and Socks.cpp

---
+++ PrepBuddy and Socks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{int t;
+cin>>t;
+while(t>0)
+{
+  int n;
+  cin>>n;
+  if(n==1)
+  cout<<"2\n";
+  else
+  cout<< n + 1 <<"\n";
+    
+  t--;
+}
+  return 0;
+}
