d4c6e67fcd0077753eead506c2dcfdd4e72205e9
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-12-19 01:03:03 +0530

    Update First Capital Using Recursion.cpp

--- First Capital Using Recursion.cpp
+++ First Capital Using Recursion.cpp
@@ -1,27 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int fc(char s[],int i)
-{ //int p=0;
-  //p=strlen(s);
-  //if(s[i]!='\0')
+{ if(s[i]!='\0')
   {if(s[i]>='A'&&s[i]<='Z')
     return i;
-  
   else
-    return  fc(s,i+1);}
- 
+    return  fc(s,i+1);
+  }
+  return -1;
 }
 int main()
 {int t;
 cin>>t;
 while(t>0)
-{char s[100];
+{char s[10000];
 int n,i=0;
 cin>>s;
 n=fc(s,i);
 cout<<n<<"\n";
   t--;
-}
-  
-  return 0;
+}return 0;
 }
