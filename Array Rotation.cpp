f8b63169531edd63fc5755b725101b12644d9c4c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-12-22 10:20:56 +0530

    Update Array Rotation.cpp

--- Array Rotation.cpp
+++ Array Rotation.cpp
@@ -1,4 +1,34 @@
-#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{int t;
+cin>>t;
+while(t>0)
+{
+  int n,k,a[1000000],b[1000000],i;
+  cin>>n>>k;
+  for(i=0;i<n;i++)
+  { cin>>a[i];}
+  for(i=0;i<n;i++)
+  {
+    b[(i+k)%n]=a[i];
+  }
+  for(i=0;i<n;i++)
+  {
+    a[i]=b[i];
+  }
+  
+  for(i=0;i<n;i++)
+  {
+    cout<<a[i]<<" ";
+  }cout<<"\n";
+  t--;
+}
+  return 0;
+}
+
+
+/*#include<iostream>
 using namespace std;
 int main()
 {
@@ -27,3 +57,4 @@ int main()
     t--;
   }
 }
+*/
