81aa72e35fa583e4401fe3015c27ecc2b260674c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-02-28 23:14:47 +0530

    Update Table Chair.cpp

--- Table Chair.cpp
+++ Table Chair.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 #include <iostream> 
 #include <algorithm>
 #include <vector> 
@@ -23,6 +23,24 @@ int main()
     cout<<std::min({r1+r1 ,r2 },comp)<<"\n";
     else if((m+3)%4==0)
     cout<<std::min({r1+r1+r1 , r2+r1 , r3 }, comp)<<"\n";
+  }
+  return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ int main()
+{long long t,n,r1,r2,r3;
+  cin>>t;
+  while(t--)
+  {
+    cin>>n>>r1>>r2>>r3;
+    n%=4;
+    if(n==0)
+    cout<<"0\n";
+    if(n==3) cout<<min(r1,min(r2+r3,r3+r3+r3))<<"\n";
+    if(n==2) cout<<min(r2,min(r1,r3)*2)<<"\n";
+    if(n==1) cout<<min(r3,min(r1+r2,r1+r1+r1))<<"\n";
   }
   return 0;
 }
