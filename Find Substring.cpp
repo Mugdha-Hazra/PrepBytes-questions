1e3761fc63f6ce7487037e2e4f46560dad35a5a1
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-03-04 07:20:07 +0530

    Create Find Substring.cpp

---
+++ Find Substring.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{int T;
+cin>>T;
+while(T--)
+{
+  string s,t;
+  cin>>s>>t;
+  int i,j,f=0;
+   if(s.size()>=t.size())
+   {
+     for(i=0;i<s.size()-t.size()&& !f;i++)
+     {
+       f=1;
+       for(j=0;j<t.size();j++)
+       {
+         if(s[i+j]!=t[j])
+         {
+           f=0;
+           break;
+         }
+       }
+     }
+   }
+   (f?cout<<"YES\n":cout<<"NO\n");
+  
+}
+  return 0;
+}
