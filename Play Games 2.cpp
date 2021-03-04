b41e0a75111e6c7281c565d64e70b5cf19a4e22a
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-03-04 12:17:46 +0530

    Create Play Games 2.cpp

---
+++ Play Games 2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    if(n%6==0||n%6==1)
+       cout<<"Friend\n";
+    else
+      cout<<"Arnab\n";   
+  }
+return 0;
+}
